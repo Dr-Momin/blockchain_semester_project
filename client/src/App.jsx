4791099e46d430bbed75280c7eee96f02951eb7d
Momin Khan <mmomin.bscsf19@iba-suk.edu.pk>
2022-11-22 02:11:41 +0500

    initial setup done...

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css';
+import {Navbar, Welcome, Footer, Services, Transactions} from "../components/index.js";
+
+
+const  App = () => {
+
+
+  return (
+    <div className={"min-h-screen"}>
+        <div className="gradient-bg-welcome">
+            <Navbar/>
+            <Welcome/>
+        </div>
+
+        <Services/>
+        <Transactions/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default App
