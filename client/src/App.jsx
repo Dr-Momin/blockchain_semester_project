bae77f5b191f137a81ea7ce2a44909f67d615da2
Momin Khan <mmomin.bscsf19@iba-suk.edu.pk>
2022-11-24 14:38:51 +0500

    frontend completed..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css';
-import {Navbar, Welcome, Footer, Services, Transactions} from "../components/index.js";
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
-
-const  App = () => {
-
-
-  return (
-    <div className={"min-h-screen"}>
-        <div className="gradient-bg-welcome">
-            <Navbar/>
-            <Welcome/>
-        </div>
-
-        <Services/>
-        <Transactions/>
-        <Footer/>
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
-  )
-}
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
